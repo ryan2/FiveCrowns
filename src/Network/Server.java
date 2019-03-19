2e9507aec573cf08471ecfd782c0a3234587aae4
ryan <ryan@Ryan>
2019-03-18 23:00:24 -0400

    Networking Additions

--- src/Network/Server.java
+++ src/Network/Server.java
@@ -4,8 +4,58 @@ import java.net.*;
 
 
 
-public class Server {
+public class Server implements Runnable {
 
 	private ServerSocket server;
+	
+	public void run() {
+		try {
+			start(5000);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public void start(int port) throws IOException {
+		server = new ServerSocket(port);
+		while (true) {
+			System.out.println("Friend!");
+			new ClientHandler(server.accept()).start();
+		}
+	}
+	
+	public void stop() throws IOException {
+		server.close();
+	}
+	
+	private static class ClientHandler extends Thread{
+		private Socket clientSocket;
+		private PrintWriter out;
+		private BufferedReader in;
+		
+		public ClientHandler(Socket socket) {
+			this.clientSocket=socket;
+		}
+		
+		public void run() {
+			try {
+				out = new PrintWriter(clientSocket.getOutputStream(),true);
+				in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+				
+				String inputLine;
+				while ((inputLine = in.readLine())!=null) {
+					out.println("Copy");
+				}
+				in.close();
+				out.close();
+				clientSocket.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
+	}
 	
 }
--- src/PiChallenge.java
+++ src/PiChallenge.java
@@ -2,9 +2,11 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+import Network.*;
+
 public class PiChallenge {
 
-	public static void main(String[] args){
+	/*public static void main(String[] args){
 		// TODO Auto-generated method stub PiDay2019 Challenge
 		System.out.println("Working R 2019");
 		boolean play = true;
@@ -28,5 +30,23 @@ public class PiChallenge {
 				play = false;
 			}
 		}
+	}
+*/
+	
+	public static void main() {;
+		Server server = new Server();
+		try {
+			(new Thread(server)).start();
+			Client client = new Client();
+			(new Thread(client)).start();
+			BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+			String response = reader.readLine();
+			String response2 = client.sendMessage(response);
+			System.out.println(response2);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 	}
 }
